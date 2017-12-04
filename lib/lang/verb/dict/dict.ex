6a342de873cb5b2c6f02cc0f1d247d8540adf16e
Warut Surapat <s.warut@gmail.com>
2017-12-05 02:40:33 +0900

    Add more function (placeholder) to dict module.

--- lib/lang/verb/dict/dict.ex
+++ lib/lang/verb/dict/dict.ex
@@ -2,8 +2,27 @@ defmodule JpKatsuyou.Verb.Dict do
   defmacro __using__(_) do
     quote do
 
-      def dic_past do
-        IO.puts("dic past")
+      def dic_past(%{kumi: "ichidan", verb: verb}) do
+        IO.puts("dic past #{verb}")
+      end
+      def dic_past(%{kumi: "godan", verb: verb}) do
+        IO.puts("dic past #{verb}")
+      end
+      def dic_past(%{kumi: "irregular", verb: verb}) do
+        IO.puts("dic past #{verb}")
+      end
+
+      def dic_present(%{kumi: "ichidan", verb: verb}) do
+        IO.puts("dic past #{verb}")
+      end
+      def dic_present(%{kumi: "godan", verb: verb}) do
+        IO.puts("dic past #{verb}")
+      end
+      def dic_present(%{kumi: "irregular", verb: verb}) do
+        IO.puts("dic past #{verb}")
+      end
+
+      def dic_present do
       end
 
     end
