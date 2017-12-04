c7e54f6eeb7b6f270cc2f5a615c708486eef23ee
Warut Surapat <s.warut@gmail.com>
2017-12-05 03:03:41 +0900

    Update version 0.1.2.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule JpKatsuYou.Mixfile do
   def project() do
     [
       app: :jp_katsuyou,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.5",
       name: "JpKatsuyou",
       description: description(),
