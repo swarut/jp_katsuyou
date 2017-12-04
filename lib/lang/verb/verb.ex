aa9fccca0ccf016b28bca09f116106b5a92468bf
Warut Surapat <s.warut@gmail.com>
2017-12-05 02:40:54 +0900

    Add verb group checking function.

--- lib/lang/verb/verb.ex
+++ lib/lang/verb/verb.ex
@@ -8,5 +8,33 @@ defmodule JpKatsuyou.Verb do
   use JpKatsuyou.Verb.Dict
   use JpKatsuyou.Verb.Polite
 
+  @special_godan_lookup %{
+    "走る" => true,
+    "要る" => true,
+    "入る" => true,
+    "知る" => true,
+    "切る" => true,
+    "返る" => true,
+    "帰る" => true,
+    "滑る" => true,
+    "蹴る" => true
+  }
+
+  def is_irregular?("する"), do: true
+  def is_irregular?("来る"), do: true
+  def is_irregular?(_),    do: false
+
+  def is_godan?(verb) do
+
+  end
+
+
+  def is_ichidan?(verb) do
+
+  end
+
+
+
+
 
 end
