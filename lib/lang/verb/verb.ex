6fedabdd4f17f856608a5571aca9833777332ed5
Warut Surapat <s.warut@gmail.com>
2017-12-05 02:58:04 +0900

    Complete the verb group checking functions.

--- lib/lang/verb/verb.ex
+++ lib/lang/verb/verb.ex
@@ -20,21 +20,37 @@ defmodule JpKatsuyou.Verb do
     "蹴る" => true
   }
 
+  @doc """
+  Check whether the supplied verb is irregular verb or not.
+
+  ## Example
+      iex> Verb.is_irregular?("する")
+      true
+  """
   def is_irregular?("する"), do: true
   def is_irregular?("来る"), do: true
   def is_irregular?(_),    do: false
 
+  @doc """
+  Check whether the supplied verb is godan verb or not.
+
+  ## Example
+      iex> Verb.is_godan?("走る")
+      true
+  """
   def is_godan?(verb) do
-
+    @special_godan_lookup[verb] || !String.ends_with?(verb, "る")
   end
 
+  @doc """
+  Check whether the supplied verb is ichidan verb or not.
 
+  ## Example
+      iex> Verb.is_ichidan?("食べる")
+      true
+  """
   def is_ichidan?(verb) do
-
+    !is_irregular?(verb) && !is_godan?(verb) && String.ends_with?(verb, "る")
   end
-
-
-
-
 
 end
